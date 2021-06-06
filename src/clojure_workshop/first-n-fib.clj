b89471e9caed205ca0ef1e1ee30cfab1dd59a6ec
Mikhail Semin <seminioni@gmail.com>
2021-06-07 02:25:36 +0500

    commit first-n-fib solution

---
+++ src/clojure_workshop/first-n-fib.clj
@@ -0,0 +1,13 @@
+(defn first-n-fib [num]
+  (letfn [(fib [i]
+            (cond
+              (= i 0) 0
+              (= i 1) 1
+              (> i 1) (+ (fib (- i 2)) (fib (- i 1))) ))
+          (gen-fib-seq [n coll]
+            (if (= n 0) coll (conj coll (gen-fib-seq (dec n) [(fib n)] )) ))]
+    (->> (gen-fib-seq num [])
+         flatten
+         reverse)))
+
+(first-n-fib 6)

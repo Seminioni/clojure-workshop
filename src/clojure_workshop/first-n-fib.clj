ebdd1caf41b0a2f4a434cf36b52698d2f563770b
Mike Syomin <seminioni@gmail.com>
2022-01-08 00:03:39 +0300

    prettify first-n-fib.clj

--- src/clojure_workshop/first-n-fib.clj
+++ src/clojure_workshop/first-n-fib.clj
@@ -3,9 +3,9 @@
             (cond
               (= i 0) 0
               (= i 1) 1
-              (> i 1) (+ (fib (- i 2)) (fib (- i 1))) ))
+              (> i 1) (+ (fib (- i 2)) (fib (- i 1)))))
           (gen-fib-seq [n coll]
-            (if (= n 0) coll (conj coll (gen-fib-seq (dec n) [(fib n)] )) ))]
+            (if (= n 0) coll (conj coll (gen-fib-seq (dec n) [(fib n)]))))]
     (->> (gen-fib-seq num [])
          flatten
          reverse)))
